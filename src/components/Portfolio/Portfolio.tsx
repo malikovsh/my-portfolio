114ad468cf3c2b9d4d53347d56ba671846de2ada
malikovsh <malikovsh.developer@gmail.com>
2025-02-12 20:40:50 +0500

    add new projects

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -5,6 +5,9 @@ import modern from '../../assets/modern.png';
 import polytex from '../../assets/polytex.png';
 import hayat from '../../assets/hayat.png';
 import kiber from '../../assets/kiber.png';
+import lacasa from '../../assets/lacasa.png';
+import banana1 from '../../assets/banana1.png';
+import banana2 from '../../assets/banana2.png';
 
 export function Portfolio() {
     return (
@@ -213,6 +216,101 @@ export function Portfolio() {
                                 <li>TypeScript</li>
                                 <li>React js</li>
                                 <li>React query</li>
+                            </ul>
+                        </footer>
+                    </div>
+                </a>
+                <a
+                    target="_blank"
+                    href="https://www.lacasa.uz/"
+                    rel="noreferrer"
+                >
+                    <div className="project">
+                        <div className="body">
+                            <img
+                                src={lacasa}
+                                style={{ width: '100%', borderRadius: '8px' }}
+                                alt=""
+                            />
+                            <h3>Lacasa Uz</h3>
+                            <p>
+                                This is a modern web application for posting
+                                real estate listings and finding potential
+                                buyers or renters. The platform allows users to
+                                add their listings, sell, or rent out
+                                properties.
+                            </p>
+                        </div>
+                        <footer>
+                            <ul className="tech-list">
+                                <li>Html</li>
+                                <li>Css</li>
+                                <li>Tailwind CSS</li>
+                                <li>JavaScript</li>
+                                <li>Firebase</li>
+                            </ul>
+                            <ul className="tech-list">
+                                <li>TypeScript</li>
+                                <li>React js</li>
+                                <li>zustand</li>
+                                <li>lint</li>
+                            </ul>
+                        </footer>
+                    </div>
+                </a>
+                <a
+                    target="_blank"
+                    href="https://play.google.com/store/apps/details?id=com.anonymous.movieapp"
+                    rel="noreferrer"
+                >
+                    <div className="project">
+                        <div className="body">
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'space-around',
+                                    border: '1px solid #f1f1f1',
+                                }}
+                            >
+                                <img
+                                    src={banana2}
+                                    style={{
+                                        width: '150px',
+                                        borderRadius: '8px',
+                                    }}
+                                    alt=""
+                                />
+                                <img
+                                    src={banana1}
+                                    style={{
+                                        width: '150px',
+                                        borderRadius: '8px',
+                                    }}
+                                    alt=""
+                                />
+                            </div>
+                            <h3>Banana Tv</h3>
+                            <p>
+                                BananaTV is a modern media platform that allows
+                                users to watch and share various movies and TV
+                                series. The app stands out with high-quality
+                                content, a user-friendly interface, and
+                                personalized recommendations.
+                            </p>
+                        </div>
+                        <footer>
+                            <ul className="tech-list">
+                                <li>React Native</li>
+                                <li>React navigation</li>
+                                <li>React hooks</li>
+                                <li>MobX</li>
+                            </ul>
+                            <ul className="tech-list">
+                                <li>Rest Api</li>
+                                <li>Axios</li>
+                                <li>JavaScript</li>
+                                <li>TypeScript</li>
+                                <li>Firebase</li>
                             </ul>
                         </footer>
                     </div>
